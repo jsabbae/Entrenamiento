5686d274bcb672f570a754d78071adf888494a77
jsabbae <juanmasb_27@hotmail.com>
2025-09-16 20:47:01 +0200

    inicio del ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,24 @@
+// Ejercicio 1: Sumar todos los números de un array
+// const numeros = [5, 10, 15, 20];
+
+
+
+
+
+// Ejercicio 2: Encontrar el número más grande
+// const numeros = [5, 10, 15, 20];
+
+
+
+// Ejercicio 3: Ordenar un array de números (de menor a mayor) usando for()
+// const numeros = [20, 5, 15, 10];
+
+
+// Ejercicio 4: Crear un nuevo array con nombres en mayúsculas
+// const usuarios = ["Ana", "Luis", "Carlos"];
+
+
+
+
+// Ejercicio 5: Filtrar números pares
+// const numeros = [1, 2, 3, 4, 5, 6];
